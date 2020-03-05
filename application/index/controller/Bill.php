fdc28b5a88d50a02f771da9e348837f6b5017e5c
lxh027 <17815540087@163.com>
2020-03-05 13:19:45 +0800

    feat: 添加订单

--- application/index/controller/Bill.php
+++ application/index/controller/Bill.php
@@ -3,8 +3,24 @@
 namespace app\index\controller;
 
 
+use app\index\model\BillModel;
+
 class Bill extends Base
 {
+
+    /*
+     * 添加订单
+     */
+    public function addBill()
+    {
+        $billModel = new BillModel();
+        $req = input('post.');
+        $req['book_date'] = date("Y-m-d H:i:s");
+        $resp = $billModel->addBill($req);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data'], 200);
+        //return $req;
+    }
+
     public function index()
     {
         return $this->fetch();
--- application/index/model/BillModel.php
+++ application/index/model/BillModel.php
@@ -2,9 +2,33 @@
 
 namespace app\index\model;
 
+use think\Exception;
+use think\exception\DbException;
 use think\Model;
 
 class BillModel extends Model
 {
     protected $table = 'bill';
+
+    /**
+     * @usage 添加订单
+     * @param array data
+     * @return array
+     */
+    public function addBill($data) {
+        try {
+            $where = ['sno' => $data['sno']];
+            $result = $this->where($where)->find();
+            if ($result) {
+                return ['code' => CODE_ERROR, 'msg' => '订单号已存在', 'data' => $result];
+            } else {
+                $this->insertGetId($data);
+                return ['code' => CODE_SUCCESS, 'msg' => '添加成功', 'data' => []];
+            }
+        } catch(DbException $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        } catch (\Exception $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        }
+    }
 }
