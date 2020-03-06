ec4ebff2074d20463171f2a8ab5bf0816814e084
lxh027 <17815540087@163.com>
2020-03-06 18:16:22 +0800

    feat: 订单列表与收款

--- application/index/controller/Bill.php
+++ application/index/controller/Bill.php
@@ -9,6 +9,50 @@ class Bill extends Base
 {
 
     /*
+     * 收款
+     */
+    public function receiveMoney()
+    {
+        $billModel = new BillModel();
+        $id = input("post.id");
+        $money = input("post.pay");
+        $resp = $billModel->receiveMoney($id, $money);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data'], 200);
+    }
+
+    /*
+     * 收款状态
+     */
+    public function moneyStatus()
+    {
+        $billModel = new BillModel();
+        $id = input("get.id");
+        $resp = $billModel->getSpecificBill(['id' => $id]);
+        return apiReturn($resp['code'], $resp['msg'], $resp['data'], 200);
+    }
+
+    /*
+     * 送货
+     */
+    public function deliverProduct()
+    {
+        $billModel = new BillModel();
+        $id = input("get.id");
+        $resp = $billModel->deliverProduct($id);
+        $this->redirect('index/bill/index');
+    }
+
+    /*
+     * 获取所有订单
+     */
+    public function getAllBill()
+    {
+        $billModel = new BillModel();
+        $resp = $billModel->getBill();
+        return apiReturn($resp['code'], $resp['msg'], $resp['data'], 200);
+    }
+
+    /*
      * 添加订单
      */
     public function addBill()
@@ -16,6 +60,10 @@ class Bill extends Base
         $billModel = new BillModel();
         $req = input('post.');
         $req['book_date'] = date("Y-m-d H:i:s");
+        $req['amount'] = round($req['amount'], 2);
+        $req['favour'] = round($req['favour'], 2);
+        $req['price'] = round($req['price'], 2);
+        $req['pay'] = round($req['pay'], 2);
         $resp = $billModel->addBill($req);
         return apiReturn($resp['code'], $resp['msg'], $resp['data'], 200);
         //return $req;
@@ -27,6 +75,21 @@ class Bill extends Base
     }
 
     public function add()
+    {
+        return $this->fetch();
+    }
+
+    public function receive()
+    {
+        return $this->fetch();
+    }
+
+    public function check()
+    {
+        return $this->fetch();
+    }
+
+    public function edit()
     {
         return $this->fetch();
     }
--- application/index/model/BillModel.php
+++ application/index/model/BillModel.php
@@ -10,6 +10,84 @@ class BillModel extends Model
 {
     protected $table = 'bill';
 
+
+    /**
+     * @usage 收款
+     * @param int id
+     * @param double money
+     * @return array
+     */
+    public function receiveMoney($id, $money) {
+        try {
+            $where = ['id' => $id];
+            $result = $this->where($where)->find();
+            if (!$result) {
+                return ['code' => CODE_ERROR, 'msg' => '订单号不存在', 'data' => $result];
+            } else {
+                if ($money<0 || $result['amount']-$result['pay']-$result['favour'] < $money) {
+                    return ['code' => CODE_ERROR, 'msg' => '超出金额范围', 'data' => []];
+                }
+                $data = [
+                    'pay' => $result['pay']+$money,
+                ];
+                $this->where($where)->update($data);
+                return ['code' => CODE_SUCCESS, 'msg' => '更新成功', 'data' => []];
+            }
+        } catch(DbException $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * @usage 送货
+     * @param int id
+     * @return array
+     */
+    public function deliverProduct($id) {
+        try {
+            $where = ['id' => $id];
+            $result = $this->where($where)->find();
+            if (!$result) {
+                return ['code' => CODE_ERROR, 'msg' => '订单号不存在', 'data' => $result];
+            } else {
+                $data = ['deliver_status' => 1];
+                $this->where($where)->update($data);
+                return ['code' => CODE_SUCCESS, 'msg' => '更新成功', 'data' => []];
+            }
+        } catch(DbException $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * @usage 获取特定订单
+     * @param array $where
+     * @return array
+     */
+    public function getSpecificBill($where) {
+        try {
+            $result = $this->where($where)->find();
+            return ['code' => CODE_SUCCESS, 'msg' => '查询成功', 'data' => $result];
+        } catch(DbException $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        }
+    }
+
+    /**
+     * @usage 获取订单
+     * @param array $where
+     * @return array
+     */
+    public function getBill($where = []) {
+        try {
+            if ($where == []) $result = $this->select();
+            else $result = $this->where($where)->select();
+            return ['code' => CODE_SUCCESS, 'msg' => '查询成功', 'data' => $result];
+        } catch(DbException $e) {
+            return ['code' => CODE_ERROR, 'msg' => '数据库异常', 'data' => $e->getMessage()];
+        }
+    }
+
     /**
      * @usage 添加订单
      * @param array data
