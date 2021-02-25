459b58228284700fce76f5a859f25861fd439315
lxh027 <992874705@qq.com>
2021-02-25 13:13:50 +0800

    add: vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6432015dcb04712cbf077ffcfb0636e37df31c2e',
+    'name' => 'topthink/think',
+  ),
+  'versions' => 
+  array (
+    'phpoffice/phpexcel' => 
+    array (
+      'pretty_version' => '1.8.2',
+      'version' => '1.8.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1441011fb7ecdd8cc689878f54f8b58a6805f870',
+    ),
+    'topthink/framework' => 
+    array (
+      'pretty_version' => 'v5.1.41',
+      'version' => '5.1.41.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7137741a323a4a60cfca334507cd1812fac91bb2',
+    ),
+    'topthink/think' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6432015dcb04712cbf077ffcfb0636e37df31c2e',
+    ),
+    'topthink/think-installer' => 
+    array (
+      'pretty_version' => 'v2.0.5',
+      'version' => '2.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38ba647706e35d6704b5d370c06f8a160b635f88',
+    ),
+  ),
+);
